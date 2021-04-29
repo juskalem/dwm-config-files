e79c6d9d71f6a15a39b08d94fc5e034a0e9b8f0d
juskalem <83347466+juskalem@users.noreply.github.com>
2021-04-29 21:47:00 +0700

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "Terminal", "Apps", "Work", "Playground", "Browser", };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
@@ -45,9 +45,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "TILE",      tile },    /* first entry is default */
-	{ "FLOAT",     NULL },    /* no layout function means floating behavior */
-	{ "MONOCLE",   monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",     NULL },    /* no layout function means floating behavior */
+	{ "",   monocle },
 };
 
 /* key definitions */
